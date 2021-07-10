ffec6cb7b0068e8d11e729d553c455d47baeaf98
Kate Kim <katehrkim@gmail.com>
2021-07-10 15:59:07 -0400

    Kate's answer

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Actor(models.Model):
+    actor_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"ACTOR: {self.actor_name}"
+
+# many to many
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=64)
+    actors = models.ManyToManyField(Actor, through="Role", related_name="movies")
+
+    def __str__(self):
+        return f"MOVIE: {self.movie_name}"
+
+# many to many, join table
+class Role(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=CASCADE, related_name="roles")
+    movie = models.ForeignKey(Movie, on_delete=CASCADE, related_name="roles")
+
+    def __str__(self):
+        return f"ROLE: {self.actor} plays in {self.movie}"
--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"USER: {self.name}"
+
+# one to many
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField
+    user = models.ForeignKey(User, related_name="post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"POST: {self.title}"
+
+# one to many w/ user and post
+class Comment(models.Model):
+    content = models.TextField
+    user = models.ForeignKey(User, related_name="comment", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comment", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"COMMENT: {self.user} said {self.content} on {self.post}"
+
